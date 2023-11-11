b6100b94a44bc1f3b7569fe98c2e185013100205
ssabrut <michael.gunawan2002@gmail.com>
2023-11-11 09:21:11 +0700

    Sidebar navigation scaffolding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,49 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>{{ config('app.name', 'Laravel') }}</title>
+
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+
+  <!-- Scripts -->
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @stack('addon-style')
+  @livewireStyles
+  <script src="https://unpkg.com/feather-icons"></script>
 </head>
 
-<body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+<body class="font-sans antialiased">
+  <div class="min-h-screen">
+    @include('layouts.navigation')
+
+    <!-- Page Heading -->
+    {{-- @if (isset($header))
+      <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          {{ $header }}
+        </div>
+      </header>
+    @endif
+
+    <!-- Page Content -->
+    <main class="container px-4 overflow-x-hidden">
+      {{ $slot }}
+    </main> --}}
+  </div>
+
+
+
+  <script>
+    feather.replace();
+  </script>
+  @stack('addon-script')
+  @livewireScripts
 </body>
 
 </html>
