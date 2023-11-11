c811a7577ff1dead900d0af108cf24e545f87842
ssabrut <michael.gunawan2002@gmail.com>
2023-11-11 09:30:08 +0700

    Navigation scaffolding

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,27 +1,2 @@
-<div class="w-96 border-r-2 h-screen">
-  <div class="mx-6 mb-4">
-    <div class="py-8 mb-6">
-      <h1 class="font-bold text-3xl">UC-Showroom</h1>
-    </div>
-    <div>
-      <div class="flex flex-col gap-4">
-        <div class="flex gap-3 items-center bg-gray-100 py-2.5 px-3.5 rounded-lg text-blue-600">
-          <i data-feather="home"></i>
-          <p class="text-xl font-medium">Dashboard</p>
-        </div>
-        <div class="flex gap-3 items-center py-2.5 px-3.5 rounded-lg">
-          <i data-feather="shopping-cart"></i>
-          <p class="text-xl font-medium">Orderan</p>
-        </div>
-        <div class="flex gap-3 items-center py-2.5 px-3.5 rounded-lg">
-          <i data-feather="user"></i>
-          <p class="text-xl font-medium">Customer</p>
-        </div>
-        <div class="flex gap-3 items-center py-2.5 px-3.5 rounded-lg">
-          <img class="w-6 h-6 object-cover" src="{{ URL::to('/') }}/icons/car.png" />
-          <p class="text-xl font-medium">Kendaraan</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+@include('partials.header')
+@include('partials.sidebar')
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header class="w-[calc(100%-384px)] absolute right-0 top-0 bg-white border-b-2"> 
+  <div class="px-8 py-8">
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  </div>
+</header>
