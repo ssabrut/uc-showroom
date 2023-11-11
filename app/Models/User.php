88f7cf8eb3c5a487e98f1976b7bdf4cd5ff4ebf7
ssabrut <michael.gunawan2002@gmail.com>
2023-11-11 09:57:39 +0700

    Add user seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,8 @@ class User extends Authenticatable {
         'phone',
     ];
 
+    protected $table = 'customers';
+
     public function orders() {
         return $this->hasMany(Order::class);
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,34 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run(): void {
+        User::create([
+            'id' => 123,
+            'name' => 'Test User',
+            'address' => '123 Test Street',
+            'phone' => '1234567890',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'id' => 456,
+            'name' => 'Haha User',
+            'address' => '123 Haha Street',
+            'phone' => '1234567890',
+        ]);
+
+        User::create([
+            'id' => 789,
+            'name' => 'Hihi User',
+            'address' => '123 Hihi Street',
+            'phone' => '1234567890',
+        ]);
     }
 }
