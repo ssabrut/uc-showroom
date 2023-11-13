ae16ce545c922777dae3f141deb2586b87f1d413
ssabrut <michael.gunawan2002@gmail.com>
2023-11-13 09:07:29 +0700

    Change dashboard screen

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,15 @@
-<header class="w-[calc(100%-384px)] fixed right-0 top-0 bg-white border-b-2 z-40"> 
+<header class="w-[calc(100%-384px)] fixed right-0 top-0 bg-white border-b-2 z-40">
   <div class="px-8 py-8">
     <div>
-      <h1>Primary : blue-600, Error : red-600, Succes : green-600</h1>
+      @if (request()->routeIs('home'))
+        <h1 class="font-semibold text-xl">Dashboard</h1>
+      @elseif(request()->routeIs('orders.*'))
+        <h1 class="font-semibold text-xl">Order</h1>
+      @elseif(request()->routeIs('customers.*'))
+        <h1 class="font-semibold text-xl">Customer</h1>
+      @elseif(request()->routeIs('vehicles.*'))
+        <h1 class="font-semibold text-xl">Vehicle</h1>
+      @endif
     </div>
   </div>
 </header>
